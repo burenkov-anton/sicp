dd68e6059e0fdeff9d72a654b76c8487eafaa13b
Anton Burenkov <buran665@yandex.ru>
2020-03-22 16:18:10 +0700

    add 2.28

---
+++ sicp/chapter2/28.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (fringe x)
+  (if (pair? (car x))
+      (let ((step (fringe-step x)))
+        (if (pair? (car step))
+            (fringe-step step)
+            step))
+      x))
+
+(define (fringe-step x)
+  (if (null? x)
+      nil
+      (if (pair? x)
+          (cons (caar x) (cons (cadar x) (fringe-step (cdr x))))
+          x)))
+
+(define x (list (list 1 2) (list 3 4)))
+(define y (list x x))
+
+(check-equal? (fringe x) (list 1 2 3 4))
+(check-equal? (fringe y) '(1 2 3 4 1 2 3 4))
