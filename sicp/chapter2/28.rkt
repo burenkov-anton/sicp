66b1c46bbc7326b2990e3c657fafbda057648e75
Anton Burenkov <buran665@yandex.ru>
2020-04-18 17:55:59 +0700

    add enumerate-tree in 2.28

--- sicp/chapter2/28.rkt
+++ sicp/chapter2/28.rkt
@@ -17,8 +17,15 @@
           (cons (caar x) (cons (cadar x) (fringe-step (cdr x))))
           x)))
 
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
 (define x (list (list 1 2) (list 3 4)))
 (define y (list x x))
 
 (check-equal? (fringe x) (list 1 2 3 4))
 (check-equal? (fringe y) '(1 2 3 4 1 2 3 4))
+(check-equal? (enumerate-tree y) '(1 2 3 4 1 2 3 4))
