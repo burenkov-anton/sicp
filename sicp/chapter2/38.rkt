a203f6ed8ca674c914abec708520929a3f3c0158
Anton Burenkov <buran665@yandex.ru>
2020-04-25 20:30:08 +0700

    add 2.38

---
+++ sicp/chapter2/38.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+
+(check-equal? (fold-right / 1 (list 1 2 3)) 3/2)
+(check-equal? (fold-left / 1 (list 1 2 3)) 1/6)
+(check-equal? (fold-right list nil (list 1 2 3)) '(1 (2 (3 ()))))
+(check-equal? (fold-left list nil (list 1 2 3)) '(((() 1) 2) 3))
+ 
