2dc9916ca9afc12c1a4e84e4210420d8d51b4dd1
Anton Burenkov <buran665@yandex.ru>
2020-05-07 10:05:31 +0700

    2.51 below via rotate

--- sicp/chapter2/51.rkt
+++ sicp/chapter2/51.rkt
@@ -170,4 +170,21 @@
         (paint-down frame)
         (paint-up frame)))))
 
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (below-via-rotate painter1 painter2)
+  (rotate270 (beside (rotate90 painter1) (rotate90 painter2))))
+  
+
 (paint (below wave wave))
