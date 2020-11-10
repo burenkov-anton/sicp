99b7ff040901dd9d089c0783db5197eceabcc42b
Anton Burenkov <buran665@yandex.ru>
2020-11-10 22:41:47 +0700

    add 3.04

---
+++ sicp/chapter3/04.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (call-the-cops) (error "Cops called"))
+
+(define (make-account balance secret)
+  (let ((pass secret))
+    (let ((attempts 0))
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch key m)
+    (cond ((eq? key secret)
+           (begin (set! attempts 0)
+                  (cond ((eq? m 'withdraw) withdraw)
+                        ((eq? m 'deposit) deposit)
+                        (else (error "Unknown request -- MAKE-ACCOUNT"
+                                     m)))))
+          (else (if (>= attempts 6)
+                           (call-the-cops)
+                           (begin
+                             (set! attempts (inc attempts))
+                             (error "Wrong password"))))))
+  dispatch)))
+
+(define acc (make-account 100 's-p))
