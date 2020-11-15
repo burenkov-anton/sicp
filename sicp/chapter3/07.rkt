2d7162880f7b6241c0de31bf97e621934021d170
Anton Burenkov <buran665@yandex.ru>
2020-11-15 17:25:21 +0700

    add 3.07

---
+++ sicp/chapter3/07.rkt
@@ -0,0 +1,39 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (make-account balance secret)
+  (let ((pass secret))
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch key m)
+    (cond ((eq? key secret)
+           (cond ((eq? m 'withdraw) withdraw)
+                 ((eq? m 'deposit) deposit)
+                 (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+          (else (error "Wrong password"))))
+  dispatch))
+
+(define (make-joint acc acc-pass new-pass)
+  (let ((pass new-pass))
+    (define (dispatch key m)
+      (cond ((eq? key new-pass) (acc acc-pass m))
+            (else (error "Wrong password"))))
+    dispatch))
+
+(define peter-acc (make-account 100 'secret-password))
+
+(define paul-acc
+    (make-joint peter-acc 'secret-password 'rosebud))
+
+(check-equal? ((peter-acc 'secret-password 'deposit) 10) 110)
+(check-equal? ((paul-acc 'rosebud 'deposit) 10) 120)
+(check-equal? ((peter-acc 'secret-password 'withdraw) 20) 100)
+(check-equal? ((paul-acc 'rosebud 'withdraw) 85) 15)
