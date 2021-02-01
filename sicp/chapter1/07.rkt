24f0f5d9f22979314ad252e2a424f942150ba8c5
Anton Burenkov <buran665@yandex.ru>
2021-02-01 12:43:55 +0700

    improve tests in 1.07

--- sicp/chapter1/07.rkt
+++ sicp/chapter1/07.rkt
@@ -20,5 +20,7 @@
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
-(check-equal? (round (sqrt 4.0)) 2.0)
-(check-equal? (round (sqrt 100.0)) 10.0)
+(check-equal? (round (* 1000 (sqrt 4.0))) 2000.0)
+(check-equal? (round (* 1000 (sqrt 100.0))) 10000.0)
+(check-equal? (round (* 1000 (sqrt 1000000.0))) 1000000.0)
+(check-equal? (round (* 1000 (sqrt 0.04))) 200.0)
