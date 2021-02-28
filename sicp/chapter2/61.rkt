144aca1703282a36652cf7ade79fbeeef4d8337d
Anton Burenkov <buran665@yandex.ru>
2021-02-28 11:20:36 +0700

    2.61 fix spaces, remove unnecessary define

--- sicp/chapter2/61.rkt
+++ sicp/chapter2/61.rkt
@@ -22,8 +22,7 @@
 (define y (adjoin-set 0 x))
 (define z (adjoin-set 1 y))
 (define q (adjoin-set 1 z))
-(define w (adjoin-set 4 q))
 
 (check-equal? z q)
-(check-equal? (adjoin-set 1(adjoin-set 0 (adjoin-set 2 '())))
-              (adjoin-set 1(adjoin-set 2 (adjoin-set 0 '()))))
+(check-equal? (adjoin-set 1 (adjoin-set 0 (adjoin-set 2 '())))
+              (adjoin-set 1 (adjoin-set 2 (adjoin-set 0 '()))))
