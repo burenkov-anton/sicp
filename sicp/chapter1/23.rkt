6cbab9797f6ba8bd1e6c8cda7d23e3d374fa2d5d
Anton Burenkov <buran665@yandex.ru>
2021-03-10 12:05:12 +0700

    add 1.23

---
+++ sicp/chapter1/23.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (square x) (* x x))
+
+(define (next x)
+  (if (= x 2)
+      3
+      (+ x 2)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(check-equal? (next 2) 3)
+(check-equal? (next 3) 5)
+(check-equal? (next 4) 6)
+(check-equal? (smallest-divisor 4) 2)
+(check-equal? (smallest-divisor 21) 3)
+(check-equal? (smallest-divisor 1999) 1999)
