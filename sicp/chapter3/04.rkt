c29b78032c905d29f99b3caa500cf6608ec06d5a
Anton Burenkov <buran665@yandex.ru>
2021-04-06 22:12:48 +0700

    add error handler

--- sicp/chapter3/04.rkt
+++ sicp/chapter3/04.rkt
@@ -1,6 +1,9 @@
-#lang sicp
+#lang racket
 
-(#%require rackunit)
+(require rackunit)
+(require racket/exn)
+
+(define (inc x) (+ x 1))
 
 (define (call-the-cops) (error "Cops called"))
 
@@ -21,11 +24,32 @@
                         ((eq? m 'deposit) deposit)
                         (else (error "Unknown request -- MAKE-ACCOUNT"
                                      m)))))
-          (else (if (>= attempts 6)
+          (else (if (>= attempts 7)
                            (call-the-cops)
                            (begin
                              (set! attempts (inc attempts))
                              (error "Wrong password"))))))
   dispatch))
 
-(define acc (make-account 100 's-p))
+(define acc (make-account 100 'secret-password))
+
+(define wrong-message "Wrong password\n")
+
+(define cops-message "Cops called\n")
+
+(define (attempt)
+  (with-handlers ([exn:fail?
+                    (lambda (e) (exn->string e))])
+     ((acc 'some-other-password 'deposit) 50)))
+
+(check-equal? ((acc 'secret-password 'withdraw) 40) 60)
+(check-equal? ((acc 'secret-password 'deposit) 40) 100)
+(check-equal? (attempt) wrong-message)
+(check-equal? (attempt) wrong-message)
+(check-equal? (attempt) wrong-message)
+(check-equal? (attempt) wrong-message)
+(check-equal? (attempt) wrong-message)
+(check-equal? (attempt) wrong-message)
+(check-equal? (attempt) wrong-message)
+(check-equal? (attempt) cops-message)
+
