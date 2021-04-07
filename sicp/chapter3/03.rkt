431915b70ee092e06a75b83e2fee1dc152d4ebc8
Anton Burenkov <buran665@yandex.ru>
2021-04-07 20:52:44 +0700

    add checks for incorrect actions

--- sicp/chapter3/03.rkt
+++ sicp/chapter3/03.rkt
@@ -1,6 +1,7 @@
-#lang sicp
+#lang racket
 
-(#%require rackunit)
+(require rackunit)
+(require racket/exn)
 
 (define (make-account balance secret)
   (define (withdraw amount)
@@ -17,10 +18,26 @@
                  ((eq? m 'deposit) deposit)
                  (else (error "Unknown request -- MAKE-ACCOUNT"
                        m))))
-          (else (error "Wrong password"))))
+          (else (error "Incorrect password"))))
   dispatch)
+
+(define incorrect-message "Incorrect password\n")
+
+(define insufficient-message "Insufficient funds")
 
 (define acc (make-account 100 'secret-password))
 
+(define (wrong-password-attempt)
+  (with-handlers ([exn:fail?
+                    (lambda (e) (exn->string e))])
+     ((acc 'some-other-password 'deposit) 50)))
+
+(define (insufficient-funds-attempt)
+  (with-handlers ([exn:fail?
+                    (lambda (e) (exn->string e))])
+     ((acc 'secret-password 'withdraw) 5000)))
+
 (check-equal? ((acc 'secret-password 'withdraw) 40) 60)
 (check-equal? ((acc 'secret-password 'deposit) 40) 100)
+(check-equal? (wrong-password-attempt) incorrect-message)
+(check-equal? (insufficient-funds-attempt) insufficient-message)
