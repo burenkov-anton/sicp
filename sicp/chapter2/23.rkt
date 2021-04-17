8bf7d30cf638a864818b0d771fb7dff734daad62
Anton Burenkov <buran665@yandex.ru>
2021-04-17 10:37:11 +0700

    handle display output in 2.23

--- sicp/chapter2/23.rkt
+++ sicp/chapter2/23.rkt
@@ -1,4 +1,6 @@
-#lang sicp
+#lang racket
+
+(require rackunit)
 
 (define (my-for-each proc list)
   (if (null? list)
@@ -6,5 +8,17 @@
       ((lambda (x) (proc (car x))
        (my-for-each proc (cdr x))) list)))
 
-(define (example-print)
-  (my-for-each (lambda (x) (newline) (display x)) (list 1 2 3 4)))
+(define (example-print example-list out)
+  (my-for-each (lambda (x) (newline) (display x out)) example-list))
+
+(define op1 (open-output-string))
+(example-print (list 1 2 3) op1)
+(define display-result (get-output-string op1))
+(check-equal? display-result "123")
+(close-output-port op1)
+
+(define op2 (open-output-string))
+(example-print (list 57 321 88) op2)
+(define display-result2 (get-output-string op2))
+(check-equal? display-result2 "5732188")
+(close-output-port op2)
