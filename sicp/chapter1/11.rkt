2982c2b7df98c70706603e90961f4498172784d2
Anton Burenkov <buran665@yandex.ru>
2021-09-22 22:11:15 +0700

    fix 1.11

--- sicp/chapter1/11.rkt
+++ sicp/chapter1/11.rkt
@@ -5,18 +5,18 @@
 (define (f x)
   (if (< x 3)
       x
-      (+ (f (- x 1)) (f (- x 2)) (f (- x 3)))))
+      (+ (f (- x 1)) (* 2 (f (- x 2))) (* 3 (f (- x 3))))))
 
 (define (f-iter x)
   (define (iter-step a b c count)
     (if (= count 0)
         c
-        (iter-step (+ a b c) a b (- count 1))))
+        (iter-step (+ a (* 2 b) (* 3 c)) a b (- count 1))))
   (if (< x 3)
       x
       (iter-step 2 1 0 x)))
 
-(check-equal? (f 5) 11)
-(check-equal? (f-iter 5) 11)
-(check-equal? (f 3) 3)
+(check-equal? (f 4) 11)
+(check-equal? (f-iter 4) 11)
+(check-equal? (f 3) 4)
 (check-equal? (f-iter 1) 1)
